ebec7b8d1a38cf9651d06fc7fd78d9ded14ce956
Jeppe Hauman <jeppe_hauman@hotmail.com>
2020-04-15 13:35:15 +0200

    Update Program.cs
    Added age to object

--- MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/Program.cs
@@ -9,8 +9,10 @@ namespace MyFirstConsoleApp
             Class1 class1 = new Class1();
 
             class1.Name = "Jeppe";
+            class1.Age = 25;
 
             Console.WriteLine(class1.Name);
+            Console.WriteLine(class1.Age);
         }
     }
 }
