62ca65941e1e1a88614cd1bdb369e8e4511c6d68
Jeppe Hauman <jeppe_hauman@hotmail.com>
2020-04-15 13:31:11 +0200

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+
+            class1.Name = "Jeppe";
+
+            Console.WriteLine(class1.Name);
+        }
+    }
+}
